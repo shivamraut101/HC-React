be4f155bcc2aa1419e8747e81e4fc9c441da07a2
shivamraut101 <shivamkumarraut12@gmail.com>
2023-09-20 19:48:00 +0530

    Created Components Login and Profile

--- Context-API/src/App.jsx
+++ Context-API/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <UserContextProvider>
+      <h1>Hey Welcome to Chai aur Code</h1>
+    </UserContextProvider>
   )
 }
 
