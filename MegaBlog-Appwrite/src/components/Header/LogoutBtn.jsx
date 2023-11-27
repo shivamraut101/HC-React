e53b33bc194416544cb3ab547e1bcfdce8ebd320
shivamraut101 <shivamkumarraut12@gmail.com>
2023-11-28 02:19:48 +0530

    Logo button component

---
+++ MegaBlog-Appwrite/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logout } from "../../store/authSlice";
+function LogoutBtn(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default LogoutBtn
