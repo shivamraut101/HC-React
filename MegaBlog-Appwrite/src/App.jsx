3698e1f072e58b240035872d6c50831227853ad9
shivamraut101 <shivamkumarraut12@gmail.com>
2023-11-30 23:47:24 +0530

    setup of app.jsx

--- MegaBlog-Appwrite/src/App.jsx
+++ MegaBlog-Appwrite/src/App.jsx
@@ -1,14 +1,42 @@
+import { useState,useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
 
-  return (
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false));
+  },[])
+
+  return !loading ? (
     <>
-     <h1>Hello World</h1>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <div>Hooo</div>
+          {/* TODO: </Outlet> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
     </>
-   
-  )
+  ): null
 }
 
 export default App
