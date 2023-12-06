8e06e31f0e6f24678cba80f165e979bf310a19f9
shivamraut101 <shivamkumarraut12@gmail.com>
2023-12-06 11:19:27 +0530

    Edit post Page

---
+++ MegaBlog-Appwrite/src/pages/EditPost.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+import { Container, PostFrom } from "../components";
+import appwriteService from "../appwrite/config";
+import { useNavigate, useParams } from "react-router-dom";
+function EditPost(){
+    const [posts, setPosts] = useState(null)
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(slug){
+            appwriteService.getPost(slug).then((post)=>{
+                if(post){
+                    setPosts(post)
+                }
+            })
+        }else{
+            navigate('/')
+        }
+    },[slug, navigate])
+
+    return posts ? (
+        <div className="py-8">
+            <Container>
+                <PostFrom post={posts} />
+            </Container>
+        </div>
+    ) : null
+}
+
+export default EditPost
