2a7b5ef85f4f447f33380aa22d41ee401feb9b95
shivamraut101 <shivamkumarraut12@gmail.com>
2023-12-10 22:06:34 +0530

    SOLVED*Bug in PostForm function {} missing

--- MegaBlog-Appwrite/src/components/post-form/PostForm.jsx
+++ MegaBlog-Appwrite/src/components/post-form/PostForm.jsx
@@ -1,102 +1,92 @@
-import React, {useCallback} from "react";
-import {Input, Select, RTE, Button} from "../index";
-import {useForm} from "react-hook-form";
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, RTE, Select } from "../index";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-function PostFrom(post){
-    const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
+export default function PostForm({ post }) {
+    const { register, handleSubmit, watch, setValue, control, getValues } = useForm({
         defaultValues: {
             title: post?.title || "",
-            slug: post?.slug || "",
+            slug: post?.$id || "",
             content: post?.content || "",
-            status: post?.status || "active"
-        }
-    })
+            status: post?.status || "active",
+        },
+    });
 
     const navigate = useNavigate();
-    const userData = useSelector(state => state.user.userData)
+    const userData = useSelector((state) => state.auth.userData);
 
     const submit = async (data) => {
-        if(post){
-            const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null;
+        if (post) {
+            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
 
-            if(file){
-                appwriteService.deleteFile(post.featuredImage)
+            if (file) {
+                appwriteService.deleteFile(post.featuredImage);
             }
 
-            const dbPost = appwriteService.updatePost(post.$id, {
+            const dbPost = await appwriteService.updatePost(post.$id, {
                 ...data,
                 featuredImage: file ? file.$id : undefined,
-            })
-            if(dbPost){
-                navigate('/post/${dbPost.$id}')
+            });
+
+            if (dbPost) {
+                navigate(`/post/${dbPost.$id}`);
             }
-              
-        }else{
-            const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null;
+        } else {
+            const file = await appwriteService.uploadFile(data.image[0]);
 
-            if(file){
-                const fileId = file.$id
-                data.featuredImage = fileId
-               const dbPost = await appwriteService.createPost({
-                    ...data,
-                    userId: userData.$id
-                })
+            if (file) {
+                const fileId = file.$id;
+                data.featuredImage = fileId;
+                const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
 
-                if(dbPost){
-                    navigate(`/post/${dbPost.$id}`)
+                if (dbPost) {
+                    navigate(`/post/${dbPost.$id}`);
                 }
             }
-
         }
-    }
+    };
 
     const slugTransform = useCallback((value) => {
-        if(value && typeof value === 'string'){
-            return value 
-            .trim()
-            .toLowerCase()
-            .replace(/^[a-zA-Z\d]/g,'-')
-            // Check this replace part if slug value is not as Required
-        }
-        else{
-            return ''
-        }
-    },[])
+        if (value && typeof value === "string")
+            return value
+                .trim()
+                .toLowerCase()
+                .replace(/[^a-zA-Z\d\s]+/g, "-")
+                .replace(/\s/g, "-");
 
-    React.useEffect(()=>{
-        const subscription = watch((value,{name})=>{
-            if(name === 'title'){
-                setValue('slug', slugTransform(value.title), {shouldValidate: true})
+        return "";
+    }, []);
+
+    React.useEffect(() => {
+        const subscription = watch((value, { name }) => {
+            if (name === "title") {
+                setValue("slug", slugTransform(value.title), { shouldValidate: true });
             }
-        })
+        });
 
-        return () =>{
-            subscription.unsubscribe()
-        }
-    },[watch,slugTransform,setValue])
+        return () => subscription.unsubscribe();
+    }, [watch, slugTransform, setValue]);
 
-    return(
-       <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+    return (
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
             <div className="w-2/3 px-2">
                 <Input
-                    label="Title: "
-                    placeholder="title"
-                    className = "mb-4"
-                    {...register("title", {required: true})}
+                    label="Title :"
+                    placeholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
                 />
                 <Input
-                    label="SLug: "
-                    placeholder="slug"
-                    className = "mb-4"
-                    {...register("slug", {required: true})}
-                    onInput = {(e) => {
-                        setValue("slug",slugTransform(e.currentTarget.value),{
-                            shouldValidate: true
-                        });
-                    } }
+                    label="Slug :"
+                    placeholder="Slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                    }}
                 />
                 <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
             </div>
@@ -127,8 +117,6 @@ function PostFrom(post){
                     {post ? "Update" : "Submit"}
                 </Button>
             </div>
-       </form>
-    )
+        </form>
+    );
 }
-
-export default PostFrom
